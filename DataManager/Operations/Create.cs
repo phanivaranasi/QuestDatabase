5534232bec0a9efb16a54e4159a950f5de4ad4b5
Phaneendra Varanasi <varanasikrishnaphaneendra@gmail.com>
2020-08-29 11:27:52 +0530

    Operation layer

---
+++ DataManager/Operations/Create.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace DataManager.Operations
+{
+    public class OperationManager<T> where T : class
+    {
+        public void Create(IDbConnection db)
+        {
+
+        }
+    }
+}
---
+++ DataManager/Utils/Utils.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataManager.Utils
+{
+   public enum DataTypes
+    {
+        MSSQL=10,
+        POSTRESQL=20,
+        MYSQL=30
+    }
+}
